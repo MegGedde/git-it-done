efb46555c257a9ce2644f1abe4a03df86981ddd4
Megan Gedde <hoglu070@d.umn.edu>
2022-04-05 22:21:57 -0500

    fixed possible errors

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -21,16 +21,31 @@ var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-        displayRepos(data, user);
-      });
+    fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data, user);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
+    })
+    .catch(function(error) {
+      // Notice this `.catch()` getting chained onto the end of the `.then()` method
+      alert("Unable to connect to GitHub");
     });
   };
 
   var displayRepos = function(repos, searchTerm) {
     console.log(repos);
     console.log(searchTerm);
+        // check if api returned any repos
+    if (repos.length === 0) {
+      repoContainerEl.textContent = "No repositories found.";
+      return;
+}
     // clear old content
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
@@ -49,7 +64,7 @@ var getUserRepos = function(user) {
 
       // append to container
       repoEl.appendChild(titleEl);
-      
+
       // create a status element
       var statusEl = document.createElement("span");
       statusEl.classList = "flex-row align-center";
