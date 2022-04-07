0350663ea982651a6e1192d0f9e60469743826cb
Megan Gedde <hoglu070@d.umn.edu>
2022-04-06 21:09:58 -0500

    added over 30 issues message

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,16 @@
 var issueContainerEl = document.querySelector("#issues-container")
+var limitWarningEl = document.querySelector("#limit-warning")
+
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
 
 var getRepoIsses = function(repo) {
     console.log(repo)
@@ -8,7 +20,11 @@ var getRepoIsses = function(repo) {
         if (response.ok) {
           response.json().then(function(data) {
             displayIssues(data)
-          });
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+                displayWarning(repo)
+            }
+            });
         }
         else {
           alert("There was a problem with your request!");
@@ -49,5 +65,7 @@ var displayIssues = function(issues) {
         issueContainerEl.appendChild(issueEl)
         }
 };
+
+
 
 getRepoIsses("facebook/react")
